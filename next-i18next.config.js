1d5b6fba91ae8562dcbf830d15c0202d102917ea
Oliver Levay <oliver.levay@hotmail.com>
2021-12-06 13:57:30 +0100

    wip

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    i18n: {
-        locales: ['sv', 'en'],
-        defaultLocale: 'sv',
-        reloadOnPrerender: process.env.NODE_ENV === "development"
-    },
-}
+  i18n: {
+    locales: ["sv", "en"],
+    defaultLocale: "sv",
+  },
+};
